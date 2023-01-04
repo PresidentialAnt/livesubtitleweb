814e8a60f121e6867dcfc6d509fdc91fd5c0d6fd
karavan11 <diva.selina20@Imperial.ac.uk>
2023-01-04 00:43:36 +0000

    change the setting page for font size and family

---
+++ src/Components/styles/fontsize.js
@@ -0,0 +1,29 @@
+<html>
+  
+<body>
+    <center>
+        <h1 style="color: green">GeeksForGeeks</h1>
+        <button type="button" name="btn1">-A</button>
+        <button type="button" name="btn2">A</button>
+        <button type="button" name="btn3">A+</button>
+        <br /><br />
+        <div style="padding: 20px; margin: 50px;
+            font-size: 20px; border: 5px solid green;"
+            id="container" class="container">
+              
+            <p>
+                A Computer Science portal for geeks. 
+                It contains well written, well thought 
+                and well explained computer ming science 
+                and programarticles,quizzes and practice/ 
+                competitive programming/company interview 
+                questions.
+            </p>
+        </div>
+  
+        <input type="range" min="10" max="30" 
+                    id="slider" value="20" />
+    </center>
+</body>
+  
+</html>
