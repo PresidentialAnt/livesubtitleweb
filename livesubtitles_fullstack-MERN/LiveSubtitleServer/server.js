9e51a442ead9b7c5d23ac8dd7c837ceb08194566
PresidentialAnt <antonio.waiming@gmail.com>
2023-01-04 15:01:52 +0000

    new users stored in accounts.json

--- livesubtitles_fullstack-MERN/LiveSubtitleServer/server.js
+++ livesubtitles_fullstack-MERN/LiveSubtitleServer/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
+const corsOptions = require('./config/cors.config')
 const mongoose = require('mongoose');
 const app = express();
 const path = require('path');
@@ -8,19 +9,6 @@ const path = require('path');
 /*
 * Middleware
 */
-
-const corsWhitelist=['http://localhost:3000','http://127.0.0.1:3000']
-const corsOptions ={ //Allows server and client to connect, otherwise will complain.
-   origin: (origin, callback) => {
-    if (corsWhitelist.indexOf(origin) !== -1|| !origin){
-        callback(null, true)
-    }else {
-        callback(new Error('Source address not allowed access'))
-    }
-   }, 
-   optionSuccessStatus:200,
-}
-
 app.use(cors(corsOptions))
 app.use(express.json())
 // app.use(express.static(path.join(__dirname, 'public')))
@@ -28,15 +16,6 @@ app.use(express.urlencoded({ extended: false }))
 app.use((err, req, res, next) => {
     res.json({ error: err.message })
 });
-
-
-/*
-* ROUTES?
-*/
-app.use('/',require('./routes/index'))
-app.use('/login', require('./routes/login'));
-app.use('/users',require('./routes/users'))
-app.use('/register', require('./routes/register'));
 
 
 /*
@@ -48,6 +27,15 @@ mongoose.connect(process.env.MONGO_URI, {
   })
   .catch(err => console.error(err))
   .then(() => console.log("Connected to mongoose server"))
+
+
+/*
+* ROUTES?
+*/
+app.use('/',require('./routes/index'))
+app.use('/login', require('./routes/login'));
+app.use('/users',require('./routes/users'))
+app.use('/register', require('./routes/register'));
 
 // world's worst routing
 require("./routes/recording.routes")(app);
